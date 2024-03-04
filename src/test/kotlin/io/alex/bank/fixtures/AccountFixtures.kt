65ba18f313248e771e67ad3bf75e499a6f1f63f3
Alex Sobolev <alex.sobolev1@gmail.com>
2024-03-04 22:41:31 +0100

    Add unit tests for AccountService (#19)

---
+++ src/test/kotlin/io/alex/bank/fixtures/AccountFixtures.kt
@@ -0,0 +1,31 @@
+package io.alex.bank.fixtures
+
+import io.alex.bank.account.models.Account
+import io.alex.bank.account.models.AccountStatus
+import io.alex.bank.account.models.AccountType
+import io.alex.bank.account.models.Currency
+import java.math.BigDecimal
+import java.time.LocalDate
+import java.util.UUID
+
+object AccountFixtures {
+    fun testAccount(
+        accountId: UUID? = UUID.randomUUID(),
+        customerId: UUID? = UUID.randomUUID(),
+        balance: BigDecimal? = BigDecimal(1000),
+        currency: Currency? = Currency.EUR,
+        type: AccountType? = AccountType.CHECKING,
+        status: AccountStatus? = AccountStatus.ACTIVE,
+        createdAt: LocalDate? = LocalDate.now(),
+        updatedAt: LocalDate? = null,
+    ) = Account(
+        id = accountId!!,
+        customerId = customerId!!,
+        balance = balance!!,
+        currency = currency!!,
+        type = type!!,
+        status = status!!,
+        createdAt = createdAt!!,
+        updatedAt = updatedAt,
+    )
+}
